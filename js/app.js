47728aa543698b95ebf5bebc70840838bb8f9b51
mursona <mursona>
2022-08-22 13:23:07 +0600

    player cardsection add

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 // h1 header text color
 const heading = document.getElementById('header-heading');
 heading.style.color="#FF5E19";
-heading.style.fontFamily= 'Berkshire Swash', cursive;
+heading.style.fontFamily= 'Berkshire Swash', "cursive";
 
 // h1 header text color
 const paragraph = document.getElementById('header-prgh');
-paragraph.style.fontFamily = 'Manrope', sans-serif;
+paragraph.style.fontFamily = 'Manrope', "sans-serif";
 
 
